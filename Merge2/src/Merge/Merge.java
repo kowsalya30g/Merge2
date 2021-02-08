81d3a2d0286611a9de90a0a6e9caa44f4c5efbca
kowsalya30g <78354123+kowsalya30g@users.noreply.github.com>
2021-02-08 13:59:11 +0530

    Update Merge.java

--- Merge2/src/Merge/Merge.java
+++ Merge2/src/Merge/Merge.java
@@ -6,6 +6,8 @@ public class Merge {
 		System.out.println("Hellooo");
 		System.out.println("Hiiiiiii");
 		System.out.println("welcome to possible");
+		System.out.println("Greetings");
+		System.out.println("hello");
 		
 	}
 
